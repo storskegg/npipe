b91d600b681a17e348143148a823e12a41c34915
Liam Conrad <liam@storskegg.org>
2017-09-13 09:01:12 -0500

    Add vendor lib, and some noop code
    * Add the go-winio vendor dependency for windows names pipes
    * Add some filler, noop code to npipe.go to show basic organization. Might think about a better abstraction, seeing as every method will have the same switch statement.

--- src/npipe/npipe.go
+++ src/npipe/npipe.go
@@ -1,1 +1,27 @@
 package npipe
+
+import (
+	"fmt"
+	"github.com/Microsoft/go-winio"
+	"runtime"
+)
+
+// Nothing really to see here, yet.
+func mkServerPipe(path string) (string, error) {
+	switch os := runtime.GOOS; os {
+	case "darwin":
+	case "freebsd":
+	case "linux":
+		// Nothing to see here.
+		break
+	case "windows":
+		pipe, err := winio.DialPipe(path, nil)
+		if err != nil {
+			return "", err
+		}
+		fmt.Println(pipe)
+	default:
+		return "", error(os + " is not yet supported.")
+	}
+	return path, nil
+}
